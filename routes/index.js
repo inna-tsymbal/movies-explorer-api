28b4627855c70b0586f61ba93b1f2a7f97fcbca5
Inna Tsymbal <inna-tsymbal2001@yandex.ru>
2024-02-03 19:41:34 +0300

    03.02

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const moviesRouter = require('./movies');
-const { createUser, login } = require('../controllers/users');
+const { addUser, login } = require('../controllers/users');
 
 const auth = require('../middlewares/auth');
 const { validateLogin, validateCreateUser } = require('../middlewares/validation');
@@ -9,7 +9,7 @@ const { validateLogin, validateCreateUser } = require('../middlewares/validation
 const NotFoundError = require('../errors/NotFoundError');
 
 router.post('/signin', validateLogin, login);
-router.post('/signup', validateCreateUser, createUser);
+router.post('/signup', validateCreateUser, addUser);
 router.use(auth);
 router.use('/', userRouter);
 router.use('/', moviesRouter);
